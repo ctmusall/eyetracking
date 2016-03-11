12c56c0a85f28b73099f1b09cf4337542787020a
Clayton Musall <claytontoddmusall@gmail.com>
2016-03-11 11:03:03 -0600

    Added UserForm and Registration Page

--- eyetracking/urls.py
+++ eyetracking/urls.py
@@ -3,8 +3,8 @@ from eyetracking import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
-    url(r'^register/', views.register, name='register'),
-    url(r'^login/', views.login, name='login'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
     url(r'^user/', views.user, name='user'),
 ]
--- eyetracking/views.py
+++ eyetracking/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from eyetracking.forms import UserForm, UserProfileForm
 
 def index(request):
     context_dict = {'boldmessage': "Please select an option below: "}
@@ -9,7 +10,36 @@ def about(request):
     return render(request, 'eyetracking/about.html')
 
 def register(request):
-    return HttpResponse("REGISTRATION PAGE")
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+                profile.save()
+
+                registered = True
+        else:
+            print user_form.errors, profile_form.errors
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+        'eyetracking/register.html',
+        {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
 
 def login(request):
     return HttpResponse("LOGIN PAGE")
