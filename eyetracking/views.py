0b6cd7b36cd823e0f949213d12c8cd8892575e24
Clayton Musall <claytontoddmusall@gmail.com>
2016-03-11 11:37:39 -0600

    Fix register bug

--- eyetracking/views.py
+++ eyetracking/views.py
@@ -30,7 +30,7 @@ def register(request):
 
                 profile.save()
 
-                registered = True
+            registered = True
         else:
             print user_form.errors, profile_form.errors
     else:
