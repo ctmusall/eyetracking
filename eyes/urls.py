b7f1c9ea7b0d33f08f31f4ccc521a637d00c4294
Clayton Musall <claytontoddmusall@gmail.com>
2016-03-14 11:11:06 -0500

    Modified media server settings

--- eyes/urls.py
+++ eyes/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import patterns, url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from eyetracking import urls
 from registration.backends.simple.views import RegistrationView
@@ -30,3 +31,6 @@ urlpatterns = [
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/',include('registration.backends.simple.urls')),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
