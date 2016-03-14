5c05611aa9842574f964d39694aefcbfa72b50ca
Clayton Musall <claytontoddmusall@gmail.com>
2016-03-13 21:15:33 -0500

    Moved user model

--- eyetracking/admin.py
+++ eyetracking/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from eyetracking.models import UserProfile, CollectedData
+from eyetracking.models import UserProfile 
 
 admin.site.register(UserProfile)
-admin.site.register(CollectedData)
--- eyetracking/models.py
+++ eyetracking/models.py
@@ -7,14 +7,5 @@ from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     picture = models.ImageField(upload_to='profile-images', blank=True)
-
     def __unicode__(self):
         return self.user.username
-
-class CollectedData(models.Model):
-    user = models.ForeignKey(UserProfile)
-    imported_date = models.DateTimeField(default = 'django.utils.timezone.now()')
-    imported_data = models.CharField(max_length = 50)
-
-    def __str__(self):
-        return self.title
